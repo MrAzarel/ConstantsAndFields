0989625ded5d4a47ca76a4ea969b77b0ea4159cf
MrAzarel <93323890+MrAzarel@users.noreply.github.com>
2022-09-28 00:33:17 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp2
+{
+    class ConstantsAndFields
+    {   
+        public const int constant = 5;
+        public readonly int[] masToRead = { 2, 3, 4, 5 };
+        private int[] masPrivate = new int[4];
+        public int[] mas3 = new int[4];
+        protected int[] mas4 = new int[4];
+        internal int[] mas5 = new int[4];
+        protected internal int[] mas6 = new int[4];
+        private protected int[] mas7 = new int[4];
+        public static int[] mas8 = new int[4];
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int constTest = ConstantsAndFields.constant;
+            constTest -= 1;
+            //Console.WriteLine(constTest);
+        }
+    }
+}
